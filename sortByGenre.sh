cea073f7b4a165cdc12cd578b37a00cf75b44d32
Hanna Sababa <hzsababa@outlook.com>
2019-07-20 22:44:11 +0300

    Add script to organize dataset by genre

---
+++ sortByGenre.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p fma_small_sorted
+cd fma_small
+for dir in $(ls) ; do
+   cd $dir
+   for file in $(ls) ; do
+		genre=$(ffprobe $file 2>&1 | grep genre | cut -d ':' -f 2 | xargs echo -n)
+		new_dir='../../fma_small_sorted/'
+		new_dir+=$genre
+		mkdir -p $new_dir # Watch out for spaces!
+		cp $file "$new_dir/$file"
+   done
+   cd ..
+done
